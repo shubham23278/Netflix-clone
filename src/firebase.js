eb0e396a2e37a1497ae435dd4a2083d76bf0bc07
Shubham Tongaria <stongaria45@gmail.com>
2021-07-05 21:56:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBvIh-SFdNW58oeAs83M0If9dok57lWB2w",
-    authDomain: "netflix-clone-24298.firebaseapp.com",
-    projectId: "netflix-clone-24298",
-    storageBucket: "netflix-clone-24298.appspot.com",
+    apiKey: "#######################",
+    authDomain: "netflix-clone-#######.firebaseapp.com",
+    projectId: "netflix-clone-######",
+    storageBucket: "netflix-clone-#######.appspot.com",
     messagingSenderId: "909015798046",
-    appId: "1:909015798046:web:dcea8632b531397a0e790f"
+    appId: "1:#########:web:dcea8632b531397a0e790f"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
